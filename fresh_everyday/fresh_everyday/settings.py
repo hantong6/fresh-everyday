45c6633a4a47894d60da6921eb9721fd791e12b2
hantong <hantong6@163.com>
2017-06-09 18:04:01 +0800

    django+uwsgi+ngix

--- fresh_everyday/fresh_everyday/settings.py
+++ fresh_everyday/fresh_everyday/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^f2j+1t_x%u1*%szo!of0gs3r1(ej9t-fgt=tcr6lrkp60%wa@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='/var/www/fresh_everyday/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
 
